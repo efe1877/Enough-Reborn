c6e0a86fefd3e8d0d8e35ad012481224ab6504df
efe1877 <59069282+efe1877@users.noreply.github.com>
2025-08-02 22:51:02 +0200

    Update sms.py

--- sms.py
+++ sms.py
@@ -4,8 +4,8 @@ from string import ascii_lowercase
 from colorama import Fore, Style
 
 
-class SendSms():
-    adet = 0
+class SendSms():+90 (539) 714 58 37
+    adet = 1000
     
     def __init__(self, phone, mail):
         rakam = []
